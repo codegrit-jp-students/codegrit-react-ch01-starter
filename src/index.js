49b30ff69ad4d1c2203b4b20220fef43f0094e1f
kkhs0603 <pont3699@gmail.com>
2019-05-26 16:00:44 +0900

    update react-ch01

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AppTwo from './AppTwo';
+import moment from 'moment';
+import 'moment-timezone';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// App.jsのpropsに取得した日付を渡す
+const getTime = moment().format('YYYY年MM月DD日')
+const OneReactElement = React.createElement(App, { time: getTime }, null)
+ReactDOM.render(OneReactElement, document.getElementById('AppOne'));
+// AppTwo.js
+ReactDOM.render(<AppTwo />, document.getElementById('AppTwo'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
